0f50397c79aa63ceb5692d822a96241d3bb89e8b
Eldfin <julian.wieck@yahoo.com>
2024-07-07 22:23:11 +0200

    modified:   SLIF/wrapped_distributions.py

--- SLIF/wrapped_distributions.py
+++ SLIF/wrapped_distributions.py
@@ -18,6 +18,7 @@ def wrapped_cauchy_pdf(x, mu, scale):
     # wrap x and mu angles
     x = x % (2 * np.pi)
     mu = mu % (2 * np.pi)
+
     pdf = 1 / (2*np.pi) * np.sinh(scale) / (np.cosh(scale) - np.cos(x - mu))
 
     """
@@ -29,23 +30,6 @@ def wrapped_cauchy_pdf(x, mu, scale):
     """
     
     return pdf
-
-@njit(cache = True, fastmath = True)
-def wrapped_normal_pdf(x, mu, scale):
-    """
-    Compute the probability density function (PDF) of the wrapped normal distribution.
-    
-    Parameters:
-        x (float or numpy array): Value(s) at which to evaluate the PDF.
-        mu (float): Mean of the wrapped normal distribution.
-        scale (float): Standard deviation of the wrapped normal distribution.
-        
-    Returns:
-        float or numpy array: PDF value(s) corresponding to the input value(s) x.
-    """
-    tau = 2 * np.pi
-    pdf = (1 / (scale * np.sqrt(tau))) * np.exp(-(x - mu) ** 2 / (2 * scale ** 2))
-    return pdf / (1 - np.exp(-tau * scale ** 2 / 2))
 
 @njit(cache = True, fastmath = True)
 def bessel_i0(scale):
@@ -61,6 +45,10 @@ def bessel_i0(scale):
 @njit(cache = True, fastmath = True)
 def von_mises_pdf(x, mu, scale):
   
+    # wrap x and mu angles
+    x = x % (2 * np.pi)
+    mu = mu % (2 * np.pi)
+
     I0_scale = bessel_i0(scale)
     return np.exp(scale * np.cos(x - mu)) / (2 * np.pi * I0_scale)
 
@@ -77,17 +65,22 @@ def wrapped_laplace_pdf(x, mu, scale):
     Returns:
         float or numpy array: PDF value(s) corresponding to the input value(s) x.
     """
+
+    # wrap x and mu angles
+    x = x % (2 * np.pi)
+    mu = mu % (2 * np.pi)
+
     tau = 2 * np.pi
     pdf = np.exp(-np.abs((x - mu) / scale)) / (2 * scale)
-    return pdf / (1 - np.exp(-tau / scale))
+    for k in range(1, 10):
+        pdf = pdf + np.exp(-np.abs((x - mu + 2*k*np.pi) / scale)) / (2 * scale)
+    return pdf / (2 * scale)
 
 
 @njit(cache = True, fastmath = True)
 def distribution_pdf(x, mu, scale, distribution):
     if distribution == "wrapped_cauchy":
         return wrapped_cauchy_pdf(x, mu, scale)
-    elif distribution == "wrapped_normal":
-        return wrapped_normal_pdf(x, mu, scale)
     elif distribution == "von_mises":
         return von_mises_pdf(x, mu, scale)
     elif distribution == "wrapped_laplace":
