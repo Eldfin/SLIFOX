3e0da179174869beeb59b210f221f3f07deb4429
Eldfin <julian.wieck@yahoo.com>
2024-11-02 14:09:00 +0100

    modified:   SLIFOX/image_generator.py

--- SLIFOX/image_generator.py
+++ SLIFOX/image_generator.py
@@ -966,8 +966,8 @@ def map_colorbar(colormap = "viridis", min_val = 0, max_val = 1, latex_unit = ""
     # Get the height of the colorbar in points
     cbar.set_ticks([])  # Remove default ticks
     # Add labels on the left and right ends
-    ax.text(0, -0.2, f'{left_border}', va='top', ha='left', transform=ax.transAxes, size=28)
-    ax.text(1, -0.2, f'{right_border}', va='top', ha='right', transform=ax.transAxes, size=28)
+    ax.text(0, -0.2, f'{min_val}{latex_unit}', va='top', ha='left', transform=ax.transAxes, size=28)
+    ax.text(1, -0.2, f'{min_val}{latex_unit}', va='top', ha='right', transform=ax.transAxes, size=28)
 
     plt.savefig(f'{directory}/{name}_colorbar.png', bbox_inches='tight', dpi=300)
 
