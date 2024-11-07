6cdf2f41918b03bb161695d687c3a0fdb982bad8
Eldfin <julian.wieck@yahoo.com>
2024-11-07 15:25:08 +0100

    modified:   SLIFOX/__init__.py

--- SLIFOX/__init__.py
+++ SLIFOX/__init__.py
@@ -4,6 +4,5 @@ from .utils import pick_data, angle_distance
 from .image_generator import plot_data_pixels, show_pixel, map_number_of_peaks, map_peak_distances, \
                     map_mean_peak_amplitudes, map_mean_peak_widths, map_directions, \
                         map_direction_significances
-from .PLI_comparison import get_distance_deviations
 from .inclination import calculate_image_inclinations
 from .fom_generator import map_fom
