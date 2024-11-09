0377f708f7274d4cc31cf3f7baafcf064db45ce7
Eldfin <julian.wieck@yahoo.com>
2024-11-09 17:41:59 +0100

    modified:   README.md 	modified:   SLIFOX/peaks_evaluator.py

--- README.md
+++ README.md
@@ -291,7 +291,7 @@ Finds all the peak_pairs for a whole image stack and sorts them by comparing wit
     "single" will only return a combination if there is only one possible (no sorting).
     "neighbor" will sort the possible peak pair combinations by neighbouring peak pairs.
     "pli" will sort by given 3d-pli measurement data by brute forcing single retardations.
-    "pli_theory" will sort by 3d-pli based on fitted heuristic formulas (fast but imprecise). 
+    "pli_theory" will sort by 3d-pli based on fitted heuristic formulas. 
     "significance" will sort the peak pair combinations by direction significance.
     "random" will sort the peak pair combinations randomly.
     Can also be a list containing multiple methods that are used in order.
--- SLIFOX/peaks_evaluator.py
+++ SLIFOX/peaks_evaluator.py
@@ -161,7 +161,7 @@ def get_image_peak_pairs(image_stack, image_params, image_peaks_mask, method = "
         "single" will only return a combination if there is only one possible (no sorting).
         "neighbor" will sort the possible peak pair combinations by neighbouring peak pairs.
         "pli" will sort by given 3d-pli measurement data by brute forcing single retardations.
-        "pli_theory" will sort by 3d-pli based on fitted heuristic formulas (fast but imprecise). 
+        "pli_theory" will sort by 3d-pli based on fitted heuristic formulas. 
         "significance" will sort the peak pair combinations by direction significance.
         "random" will sort the peak pair combinations randomly.
         Can also be a list containing multiple methods that are used in order.
